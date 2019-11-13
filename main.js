752c0dc4b14f749a4f7cce7ad5b5799ca5cc1c9f
IzabellaMartirosyan <izabell_martirosyan@edu.aua.am>
2019-11-13 14:40:16 +0400

    added finding primes from interval function

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 const reader = require('readline-sync');
-const utils = require('./myUtils');
+const utils = require('./utils');
 
-console.log(utils.isPrime(11));
+const n1 = reader.questionInt("Please insert a positive number = ");
+const n2 = reader.questionInt("Please insert a positive number = ");
+
+function primes(a, b) {
+  if(a > 0 && b > 0){
+    let primeNumbers = [];
+
+    for(let i = a; i <= b; i++){
+      if(utils.isPrime(i)){
+        primeNumbers.push(i)
+      }
+    }
+    if(primeNumbers.length){
+      return primeNumbers;
+    }else{
+      return "there are no prime numbers in the interval";
+    }
+  }else{
+    return "both numbers should be positive";
+  }
+}
+
+let val = primes(n1,n2);
+util.printArrayElements(val);
